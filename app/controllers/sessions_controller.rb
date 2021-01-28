5492a5d080c3496dae788ce79243fae423af2e02
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-28 15:27:21 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    def new 
+
+    end
+
+    def create 
+        # binding.pry 
+        @user = User.find_by(name: params[:user][:name])
+        authenticated = @user.try(:authenticate, params[:user][:password])
+        if authenticated
+            session[:user_id] = @user.id 
+            redirect_to '/'
+        else
+            redirect_to(controller: 'users', action: 'new')
+        end 
+
+  
+    end
+
+    def destroy 
+        session.delete :user_id
+        redirect_to '/login'
+    end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+    before_action :required_logged_in 
+    
+    def home 
+
+    end
+end
